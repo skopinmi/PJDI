04f0ebfc5444b36e3adf9046e5ceaa79ab6ab294
Skopin <Skopin_MI@mail.ru>
2021-03-24 21:08:47 +0300

    DZ6

---
+++ simple-app/src/main/java/ru/geekbrains/MainController.java
@@ -0,0 +1,31 @@
+package ru.geekbrains;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MainController {
+
+    Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @GetMapping("/hello")
+    public String indexPage () {
+        return "index";
+    }
+
+    @GetMapping("/get")
+    public String getPage () {
+        logger.info("get");
+        return "get";
+    }
+
+    @PostMapping("/post")
+    public String postPage () {
+        logger.info("post");
+        return "post";
+    }
+}
