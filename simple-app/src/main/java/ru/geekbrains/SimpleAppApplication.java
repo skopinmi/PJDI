ffc87d7ddd03ee59e3645dcdb21fafe26e7c5b19
Skopin <Skopin_MI@mail.ru>
2021-03-29 23:26:42 +0300

    DZ7

--- simple-app/src/main/java/ru/geekbrains/SimpleAppApplication.java
+++ simple-app/src/main/java/ru/geekbrains/SimpleAppApplication.java
@@ -2,12 +2,20 @@ package ru.geekbrains;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class SimpleAppApplication {
+public class SimpleAppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleAppApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SimpleAppApplication.class);
     }
 
 }
--- simple-app/src/main/java/ru/geekbrains/controllers/MainController.java
+++ simple-app/src/main/java/ru/geekbrains/controllers/MainController.java
@@ -1,9 +1,8 @@
-package ru.geekbrains;
+package ru.geekbrains.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
